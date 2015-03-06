3b71c1556378871e5dfc4f90d60944e75c388d1e
Nicholas Busby <Nicholas.Busby@Live.com>
2015-03-05 20:21:15 -0500

    getting files in place.

---
+++ Keeper/Keeper/VisualControlers/Player/Player.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keeper.VisualControlers.Player {
+    class Player : IVisualControl {
+
+        public Texture2D texture { get; set; }
+        public Vector2 position { get; set; }
+        public bool active { get; set; }
+
+        public void Initialize(Microsoft.Xna.Framework.Graphics.Texture2D texture, Microsoft.Xna.Framework.Vector2 position) {
+            this.position = position;
+            this.texture = texture;
+            this.active = true;
+        }
+
+        public void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch) {
+            spriteBatch.Draw(texture, position, null, Color.White, 0f, Vector2.Zero, 1f,
+               SpriteEffects.None, 0f);
+        }
+
+        public void Update(Microsoft.Xna.Framework.GameTime gameTime) {
+            
+        }
+    }
+}
