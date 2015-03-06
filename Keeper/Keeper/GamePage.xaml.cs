8843853fdc0455a5f0cfd593ba185ccee0d510d9
Nicholas Busby <Nicholas.Busby@Live.com>
2015-03-05 20:10:19 -0500

    push of project.

---
+++ Keeper/Keeper/GamePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using MonoGame.Framework;
+using Windows.ApplicationModel.Activation;
+
+
+namespace Keeper
+{
+    public sealed partial class GamePage : SwapChainBackgroundPanel
+    {
+        readonly Keeper _game;
+
+        public GamePage(LaunchActivatedEventArgs args)
+        {
+            this.InitializeComponent();
+
+            _game = XamlGame<Keeper>.Create(args, Window.Current.CoreWindow, this);
+        }
+    }
+}
