627d834185918c4f306e3452751d08ad95971e67
Nicholas Busby <Nicholas.Busby@Live.com>
2015-03-09 19:49:26 -0400

    got some movement working but not quite all of it

--- Keeper/Keeper/Keeper.cs
+++ Keeper/Keeper/Keeper.cs
@@ -1,4 +1,5 @@
-﻿using Keeper.VisualControlers.Player;
+﻿using Keeper.VisualControlers.Controls;
+using Keeper.VisualControlers.Player;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -9,6 +10,8 @@ namespace Keeper
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Player player;
+        Joystick leftStick;
+        Joystick rightStick;
 
         public Keeper()
         {
@@ -19,6 +22,8 @@ namespace Keeper
         protected override void Initialize()
         {
             player = new Player();
+            leftStick = new Joystick();
+            rightStick = new Joystick();
             base.Initialize();
         }
 
@@ -26,7 +31,14 @@ namespace Keeper
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
-            player.Initialize(Content.Load<Texture2D>("Graphics\\player"), playerPosition); 
+
+            float joyStickY = GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height - 500;
+            Vector2 leftJoystickPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, joyStickY);
+            Vector2 rightJoystickPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X + GraphicsDevice.Viewport.TitleSafeArea.Width - 500, joyStickY);
+
+            player.Initialize(Content.Load<Texture2D>("Graphics\\player"), playerPosition);
+            leftStick.Initialize(Content.Load<Texture2D>("Graphics\\joyStick"), leftJoystickPosition);
+            rightStick.Initialize(Content.Load<Texture2D>("Graphics\\joyStick"), rightJoystickPosition);
         }
 
         protected override void UnloadContent()
@@ -36,6 +48,9 @@ namespace Keeper
 
         protected override void Update(GameTime gameTime)
         {
+            leftStick.Update(gameTime);
+            rightStick.Update(gameTime);
+            player.Update(gameTime, leftStick.rotationOutput, leftStick.length, rightStick.rotationOutput);
             base.Update(gameTime);
         }
 
@@ -44,6 +59,9 @@ namespace Keeper
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
             player.Draw(spriteBatch);
+
+            leftStick.Draw(spriteBatch);
+            rightStick.Draw(spriteBatch);
             spriteBatch.End(); 
             base.Draw(gameTime);
         }
--- Keeper/Keeper/VisualControlers/Player/Player.cs
+++ Keeper/Keeper/VisualControlers/Player/Player.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input.Touch;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,25 +8,39 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Keeper.VisualControlers.Player {
-    class Player : IVisualControl {
+    class Player {
 
         public Texture2D texture { get; set; }
         public Vector2 position { get; set; }
         public bool active { get; set; }
+        private float rotation = 0f;
+        private double speed = 0;
 
-        public void Initialize(Microsoft.Xna.Framework.Graphics.Texture2D texture, Microsoft.Xna.Framework.Vector2 position) {
+        public void Initialize(Texture2D texture, Vector2 position) {
             this.position = position;
             this.texture = texture;
             this.active = true;
         }
 
-        public void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch) {
-            spriteBatch.Draw(texture, position, null, Color.White, 0f, Vector2.Zero, 1f,
+        public void Draw(SpriteBatch spriteBatch) {
+            spriteBatch.Draw(texture, position, null, Color.White, rotation, Vector2.Zero, 1f,
                SpriteEffects.None, 0f);
         }
 
-        public void Update(Microsoft.Xna.Framework.GameTime gameTime) {
-            
+        public void Update(GameTime gameTime, float movementAngle, float movementSpeed, float shootAngle) {
+            if (movementSpeed > 0) {
+                rotation = movementAngle;
+                speed = movementSpeed;
+                Vector2 movement = angleToVector(movementAngle);
+                //movement = Vector2.Multiply(movement, movementSpeed);
+                position = Vector2.Add(position, movement);
+            } else {
+                movementSpeed = 0;
+            }
+        }
+
+        private Vector2 angleToVector(float angle) {
+            return new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
         }
     }
 }
